9e9d1ae53d69ebec86d57d66fb6710303333e09d
Anthony Lopez <anthonydlr66@gmail.com>
2023-04-02 21:35:55 -0400

    splice is annoying

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const people = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise"
+
+function writeCards(people, event) {
+    const copyofPeople = [...people]
+    copyofPeople.splice(0,3,"Thank you, Guadalupe, for the wonderful surprise gift!", "Thank you, Ollie, for the wonderful surprise gift!", "Thank you, Aki, for the wonderful surprise gift!")
+    return copyofPeople
+}
+
+writeCards
+
+const b = 4
+
+function countDown(b) {
+    while (b >= 0) {
+        console.log(b--);
+    }
+    return b
+}
+
+countDown(b)
